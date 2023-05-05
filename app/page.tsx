61d98d11a34f25f13eff3c7d0142b7e4401ff632
Leon San José Larsson <leonlarsson8@gmail.com>
2023-05-05 23:50:07 +0200

    add URL icons and change style

--- app/page.tsx
+++ app/page.tsx
@@ -7,17 +7,17 @@ export default () => {
     <div className="flex flex-col items-center justify-center h-screen text-center bg-slate-100 px-4">
 
       <Image className="rounded-full mb-3 shadow-xl max-[420px]:w-2/5" src="/assets/images/avatar.png" alt="Leon image" width={150} height={150} priority />
-      <span className="font-extrabold text-[2rem] leading-none transition-all max-[420px]:text-[1.5rem]">Leon San José Larsson</span>
-      <span className="text-lg font-semibold italic transition-all max-[420px]:text-sm">Community & Support Specialist</span>
-      <a className="font-extrabold text-[1.8rem] hover:text-[rgb(231,51,49)] transition-all max-[420px]:text-[1.5rem]  hover:underline" href="https://sharkmob.com">Sharkmob</a>
+      <span className="font-extrabold text-[2rem] leading-none max-[420px]:text-[1.5rem]">Leon San José Larsson</span>
+      <span className="text-lg font-semibold italic max-[420px]:text-sm">Community & Support Specialist</span>
+      <a className="font-extrabold text-[1.8rem] hover:text-[rgb(231,51,49)] transition-colors duration-300 max-[420px]:text-[1.5rem] hover:underline" href="https://sharkmob.com">Sharkmob</a>
 
-      <div className="flex justify-center flex-wrap gap-x-2 max-[420px]:border-b-4 border-b-slate-900">
-        <ProfileLink title="Email" url="mailto:leonlarsson8@gmail.com" />
-        <ProfileLink title="LinkedIn" url="https://www.linkedin.com/in/leonlarsson/" />
-        <ProfileLink title="Twitter" url="https://twitter.com/MozzyFX" />
-        <ProfileLink title="GitHub" url="https://github.com/leonlarsson" />
-        <ProfileLink title="Behance" url="https://www.behance.net/leonlarsson" />
-        <ProfileLink title="ArtStation" url="https://www.artstation.com/leonlarsson" />
+      <div className="flex justify-center flex-wrap gap-x-1 max-[420px]:border-b-4 border-b-slate-900">
+        <ProfileLink title="Email" url="mailto:leonlarsson8@gmail.com" icon={<i className="fa-solid fa-envelope" />} />
+        <ProfileLink title="LinkedIn" url="https://www.linkedin.com/in/leonlarsson/" icon={<i className="fa-brands fa-linkedin" />} />
+        <ProfileLink title="Twitter" url="https://twitter.com/MozzyFX" icon={<i className="fa-brands fa-twitter" />} />
+        <ProfileLink title="GitHub" url="https://github.com/leonlarsson" icon={<i className="fa-brands fa-github" />} />
+        <ProfileLink title="Behance" url="https://www.behance.net/leonlarsson" icon={<i className="fa-brands fa-behance" />} />
+        <ProfileLink title="ArtStation" url="https://www.artstation.com/leonlarsson" icon={<i className="fa-brands fa-artstation" />} />
       </div>
 
       <div className="flex justify-center flex-wrap gap-x-2">
@@ -29,5 +29,5 @@ export default () => {
   );
 };
 
-const ProfileLink = ({ title, url, newTab = true }: { title: string, url: string, newTab?: boolean }) => <a className="home-link" href={url} target={newTab ? "_blank" : "_self"}>{title}</a>
+const ProfileLink = ({ title, url, icon, newTab = true }: { title: string, url: string, icon?: React.ReactNode, newTab?: boolean }) => <a className="home-link" href={url} target={newTab ? "_blank" : "_self"}>{icon} {title}</a>
 
