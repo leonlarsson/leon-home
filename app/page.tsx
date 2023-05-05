694aee2d3e14d0e57426ce56680ed803f13daf99
Leon San José Larsson <leonlarsson8@gmail.com>
2023-05-06 00:00:55 +0200

    update company title layout
    Also fine-tunes the added border

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,9 @@ export default () => {
 
       <Image className="rounded-full mb-3 shadow-xl max-[420px]:w-2/5" src="/assets/images/avatar.png" alt="Leon image" width={150} height={150} priority />
       <span className="font-extrabold text-[2rem] leading-none max-[420px]:text-[1.5rem]">Leon San José Larsson</span>
-      <span className="text-lg font-semibold italic max-[420px]:text-sm">Community & Support Specialist</span>
-      <a className="font-extrabold text-[1.8rem] hover:text-[rgb(231,51,49)] transition-colors duration-300 max-[420px]:text-[1.5rem] hover:underline" href="https://sharkmob.com">Sharkmob</a>
+      <span className="text-lg font-semibold italic max-[420px]:text-sm">Community & Support Specialist @ <a href="https://sharkmob.com" target="_blank" className="hover:text-[rgb(231,51,49)] transition-colors duration-300">Sharkmob</a></span>
 
-      <div className="flex justify-center flex-wrap gap-x-1 max-[420px]:border-b-4 border-b-slate-900">
+      <div className="flex justify-center flex-wrap gap-x-1 mt-2 max-[632px]:border-b-4 border-b-slate-900">
         <ProfileLink title="Email" url="mailto:leonlarsson8@gmail.com" icon={<i className="fa-solid fa-envelope" />} />
         <ProfileLink title="LinkedIn" url="https://www.linkedin.com/in/leonlarsson/" icon={<i className="fa-brands fa-linkedin" />} />
         <ProfileLink title="Twitter" url="https://twitter.com/MozzyFX" icon={<i className="fa-brands fa-twitter" />} />
