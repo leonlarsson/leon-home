61aae014461b32acd97673f57e0339fcbf510bba
Leon San José Larsson <leonlarsson8@gmail.com>
2023-05-06 20:40:20 +0200

    use ts for TW config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
   content: [
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   plugins: [],
-}
+} satisfies Config;
