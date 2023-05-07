6750fd7fdd23ff2b029755cdc7ef05f9069ee595
Leon San José Larsson <leonlarsson8@gmail.com>
2023-05-07 18:33:07 +0200

    projects: add dynamic OG image
    Vercel will likely complain about having 2 fonts...

---
+++ app/projects/[id]/opengraph-image.tsx
@@ -0,0 +1,60 @@
+import { ImageResponse } from "next/server";
+import projects from "../projects";
+
+export const runtime = "edge";
+// export const size = { width: 896, height: 605 };
+
+const regularFont = fetch(
+    new URL("/public/assets/fonts/Inter-Regular.ttf", import.meta.url)
+).then(res => res.arrayBuffer());
+
+const boldFont = fetch(
+    new URL("/public/assets/fonts/Inter-Bold.ttf", import.meta.url)
+).then(res => res.arrayBuffer());
+
+export default async ({ params }: { params: { id: string } }) => {
+
+    const [regularFontData, boldFontData] = await Promise.all([regularFont, boldFont]);
+
+    const project = projects.find(project => project.projectId === parseInt(params.id));
+
+    return new ImageResponse(
+        (
+            <div
+                style={{
+                    height: "100%",
+                    width: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    textAlign: "center",
+                    backgroundColor: "#fff",
+                    fontWeight: 400,
+                    whiteSpace: "pre-line"
+                }}
+            >
+                <div style={{ fontSize: 40, marginBottom: 10 }}>Project</div>
+                <div style={{ fontSize: 50, fontWeight: 900 }}>{project?.name ?? "Project #404"}</div>
+                <div style={{ fontSize: 26, padding: 30 }}>{project?.description ?? "You found Project #404"}</div>
+            </div>
+
+        ),
+        {
+            width: 896,
+            height: 605,
+            fonts: [
+                {
+                    name: "Inter",
+                    data: regularFontData,
+                    weight: 400
+                },
+                {
+                    name: "Inter",
+                    data: boldFontData,
+                    weight: 700
+                }
+            ]
+        }
+    );
+}
