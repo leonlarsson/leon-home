c7f8fd187dba2cef392d616d5b30ca7ee0aebb13
Leon San José Larsson <leonlarsson8@gmail.com>
2023-05-07 22:54:13 +0200

    projects:  adjust padding for OG description

--- app/projects/[id]/opengraph-image.tsx
+++ app/projects/[id]/opengraph-image.tsx
@@ -18,7 +18,7 @@ export default async ({ params }: { params: { id: string } }) => {
                 <img tw="rounded-full shadow-xl" src={avatarData} width={100} height={100} />
                 <div tw="text-[30px] mb-2">Project</div>
                 <div tw="text-[50px] font-[900]">{project?.name ?? "Project #404"}</div>
-                <div tw="text-[26px] p-8">{project?.description ?? "You found Project #404."}</div>
+                <div tw="text-[26px] px-8">{project?.description ?? "You found Project #404."}</div>
                 <div tw="flex absolute top-[565px] left-[15px] text-lg">leonlarsson.com/projects/{params.id.slice(0, 30)}</div>
             </div>
         ),
