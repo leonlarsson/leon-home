c64881babd956604735be53d2fe32cbfe8d8e2ad
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2023-05-10 16:43:12 +0200

    project: use m-auto to center

--- app/(main)/projects/[id]/page.tsx
+++ app/(main)/projects/[id]/page.tsx
@@ -56,6 +56,7 @@ export default ({ params }: { params: { id: string } }) => {
               </Link>
             )}
           </span>
+
           <p className="mb-3 whitespace-pre-line">{project.description}</p>
 
           <span className="text-lg font-bold">Links:</span>
@@ -88,7 +89,7 @@ export default ({ params }: { params: { id: string } }) => {
             )}
           </div>
 
-          <details className="my-5 rounded border border-black p-2 transition-colors open:bg-black open:text-white hover:bg-black hover:text-white max-xl:min-w-[70vw] lg:w-[80vw] 2xl:w-[1300px]">
+          <details className="m-auto my-5 rounded border border-black p-2 transition-colors open:bg-black open:text-white hover:bg-black hover:text-white max-xl:min-w-[70vw] lg:w-[80vw] 2xl:w-[1300px]">
             <summary className="cursor-pointer text-lg font-semibold">Try it {project.projectId === 4 && "(Inception style)"}</summary>
             <iframe src={project.link} className="h-[500px] w-full rounded bg-white lg:h-[500px] xl:h-[700px]"></iframe>
           </details>
