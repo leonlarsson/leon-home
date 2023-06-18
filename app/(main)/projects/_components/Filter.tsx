d24c5527287856e8805b495fa05ed3915fdb0951
Leon San José Larsson <leonlarsson8@gmail.com>
2023-06-18 23:21:47 +0200

    improve Filter component

--- app/(main)/projects/_components/Filter.tsx
+++ app/(main)/projects/_components/Filter.tsx
@@ -1,8 +1,15 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default () => {
   const router = useRouter();
-  return <input type="search" placeholder="Filter" className="rounded p-2 shadow outline-none" onChange={e => router.replace(`?filter=${e.target.value}`)} />;
+  const searchParams = new URLSearchParams(useSearchParams().toString());
+
+  const handleChange = (filter: string) => {
+    filter.length ? searchParams.set("filter", filter) : searchParams.delete("filter");
+    router.replace("?" + searchParams.toString());
+  };
+
+  return <input type="search" placeholder="Filter" className="w-60 self-center rounded p-2 shadow outline-none" onChange={e => handleChange(e.target.value)} />;
 };
