be2aaab4589da917a36107ffe3aff8ae0adf9ccf
Leon San José Larsson <leonlarsson8@gmail.com>
2023-06-19 00:15:04 +0200

    Filter: use state instead

--- app/(main)/projects/_components/Filter.tsx
+++ app/(main)/projects/_components/Filter.tsx
@@ -1,21 +1,23 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
 
 export default () => {
-  const input = useRef<HTMLInputElement>(null);
+  const [filterQuery, setFilterQuery] = useState("");
   const router = useRouter();
   const searchParams = new URLSearchParams(useSearchParams().toString());
 
-  const handleChange = (filter: string) => {
-    filter.length ? searchParams.set("filter", filter) : searchParams.delete("filter");
-    router.replace("?" + searchParams.toString());
-  };
-
+  // When the state changes, set or remove the searchParams
   useEffect(() => {
-    if (searchParams.size === 1) input.current!.value = "";
-  }, [searchParams]);
+    filterQuery.trim().length ? searchParams.set("filter", filterQuery.trim()) : searchParams.delete("filter");
+    router.replace("?" + searchParams.toString());
+  }, [filterQuery]);
 
-  return <input type="search" placeholder="Name, description, tags..." className="w-60 self-center rounded border border-black/50 p-2 shadow outline-none" onChange={e => handleChange(e.target.value.trim())} ref={input} />;
+  // Reset state when layout changes
+  useEffect(() => {
+    setFilterQuery("");
+  }, [searchParams.get("grid")]);
+
+  return <input type="search" placeholder="Name, description, tags..." className="w-60 self-center rounded border border-black/50 p-2 shadow outline-none" value={filterQuery} onChange={e => setFilterQuery(e.target.value)} />;
 };
