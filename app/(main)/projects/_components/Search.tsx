291c20a862364be691dd0723f31541a15306525a
Leon San José Larsson <leonlarsson8@gmail.com>
2023-06-19 17:33:24 +0200

    Search: allow for sharing links

--- app/(main)/projects/_components/Search.tsx
+++ app/(main)/projects/_components/Search.tsx
@@ -8,16 +8,31 @@ export default () => {
   const router = useRouter();
   const searchParams = new URLSearchParams(useSearchParams().toString());
 
-  // When the state changes, set or remove the searchParams
-  useEffect(() => {
-    searchQuery.trim().length ? searchParams.set("search", searchQuery.trim()) : searchParams.delete("search");
-    router.replace("?" + searchParams.toString());
-  }, [searchQuery]);
-
   // Reset state when layout changes
   useEffect(() => {
     setSearchQuery("");
   }, [searchParams.get("grid")]);
 
-  return <input type="search" maxLength={20} placeholder="Name, description, tags..." className="w-full self-center rounded border border-black/50 bg-white p-1 shadow outline-none md:w-80" value={searchQuery} onChange={e => setSearchQuery(e.target.value)} />;
+  // On mount, set the state from searchParam
+  useEffect(() => {
+    const searchParam = searchParams.get("search");
+    if (searchParam) setSearchQuery(searchParam);
+  }, []);
+
+  return (
+    <input
+      type="search"
+      maxLength={20}
+      placeholder="Name, description, tags..."
+      className="w-full self-center rounded border border-black/50 bg-white p-1 shadow outline-none md:w-80"
+      value={searchQuery}
+      onChange={e => {
+        const value = e.target.value;
+        setSearchQuery(value);
+        // If value is not empty, set searchParam. Otherwise, remove the searchParam. Then replace the URL params
+        value.trim().length ? searchParams.set("search", value.trim()) : searchParams.delete("search");
+        router.replace("?" + searchParams.toString());
+      }}
+    />
+  );
 };
