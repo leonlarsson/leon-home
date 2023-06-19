b6b52d7d2f1f50951a9c9bb78bbd0a65828062e7
Leon San José Larsson <leonlarsson8@gmail.com>
2023-06-19 14:13:40 +0200

    Search: responsive width

--- app/(main)/projects/_components/Search.tsx
+++ app/(main)/projects/_components/Search.tsx
@@ -19,5 +19,5 @@ export default () => {
     setSearchQuery("");
   }, [searchParams.get("grid")]);
 
-  return <input type="search" maxLength={20} placeholder="Name, description, tags..." className="w-60 self-center rounded border border-black/50 bg-white p-1 shadow outline-none" value={searchQuery} onChange={e => setSearchQuery(e.target.value)} />;
+  return <input type="search" maxLength={20} placeholder="Name, description, tags..." className="w-full self-center rounded border border-black/50 bg-white p-1 shadow outline-none md:w-80" value={searchQuery} onChange={e => setSearchQuery(e.target.value)} />;
 };
