c8c702c795848724b4039bfb2ecf8d4572197c3e
Leon San José Larsson <leonlarsson8@gmail.com>
2023-06-26 01:02:41 +0200

    revert tests

--- app/(main)/guestbook/_components/Entries.tsx
+++ app/(main)/guestbook/_components/Entries.tsx
@@ -1,5 +1,5 @@
 import { profanity } from "@2toad/profanity";
-// import DeleteEntryButton from "./DeleteEntryButton";
+import DeleteEntryButton from "./DeleteEntryButton";
 import { getEntries } from "../apiActions";
 
 export default async ({ admin }: { admin: boolean }) => {
@@ -9,10 +9,9 @@ export default async ({ admin }: { admin: boolean }) => {
 
   return (
     <section className="flex flex-col">
-      {entries.slice(0, 2).map(entry => (
+      {entries.map(entry => (
         <div key={entry.id} className="select-text break-words rounded p-1 text-left text-sm hover:bg-gray-300">
-          {profanity.censor(entry.body)}
-          {/* <b>{entry.name ?? "Anonymous"}:</b> <span className="break-all">{entry.body}</span> */}
+          {admin && <DeleteEntryButton id={entry.id} />} <b title={new Date(entry.date).toUTCString()}>{entry.name ?? "Anonymous"}:</b> <span className="break-all">{profanity.censor(entry.body)}</span>
         </div>
       ))}
       {!entries.length && <span>Awaiting messages...</span>}
