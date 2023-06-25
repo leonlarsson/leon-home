b66bb3b919e7c37d843719c974b347ddcf1a2d27
Leon San José Larsson <leonlarsson8@gmail.com>
2023-06-26 00:36:26 +0200

    revert all tests and alternative picker

--- app/(main)/guestbook/_components/EmojiPicker.tsx
+++ app/(main)/guestbook/_components/EmojiPicker.tsx
@@ -1,26 +1,30 @@
-// "use client";
+"use client";
 
-// import Picker from "@emoji-mart/react";
+import dynamic from "next/dynamic";
+import { EmojiStyle } from "emoji-picker-react";
 
-// import dynamic from "next/dynamic";
-// import { EmojiStyle } from "emoji-picker-react";
+const EmojiPicker = dynamic(() => import("emoji-picker-react"), { ssr: false });
 
-// const EmojiPicker = dynamic(() => import("emoji-picker-react"));
+export default ({ isMutating, postEntryFunc }: { isMutating: boolean; postEntryFunc: (message: string) => Promise<boolean> }) => {
+  // npm i @emoji-mart/react
+  // <Picker theme="light" dynamicWidth={true} skinTonePosition="none" categories={["people", "nature", "foods", "activity", "places", "objects", "symbols", "flags"]} onEmojiSelect={(e: { native: string }) => !isMutating && postEntryFunc(e.native)} />
+  //  em-emoji-picker {
+  //   height: 400px;
+  //   width: 100%;
+  // }
 
-// export default ({ isMutating, postEntryFunc }: { isMutating: boolean; postEntryFunc: (message: string) => Promise<boolean> }) => {
-//   return (
-//     <Picker theme="light" dynamicWidth={true} skinTonePosition="none" categories={["people", "nature", "foods", "activity", "places", "objects", "symbols", "flags"]} onEmojiSelect={(e: { native: string }) => !isMutating && postEntryFunc(e.native)} />
-// <EmojiPicker
-//   onEmojiClick={e => !isMutating && postEntryFunc(e.emoji)}
-//   lazyLoadEmojis
-//   skinTonesDisabled
-//   emojiStyle={EmojiStyle.NATIVE}
-//   emojiVersion="5.0"
-//   previewConfig={{ defaultEmoji: "1f47d" }}
-//   height={400}
-//   width={"100%"}
-//   // @ts-expect-error
-//   categories={["smileys_people", "animals_nature", "food_drink", "travel_places", "activities", "objects", "symbols", "flags"]}
-// />
-//   );
-// };
+  return (
+    <EmojiPicker
+      onEmojiClick={e => !isMutating && postEntryFunc(e.emoji)}
+      lazyLoadEmojis
+      skinTonesDisabled
+      emojiStyle={EmojiStyle.NATIVE}
+      emojiVersion="5.0"
+      previewConfig={{ defaultEmoji: "1f47d" }}
+      height={400}
+      width={"100%"}
+      // @ts-expect-error
+      categories={["smileys_people", "animals_nature", "food_drink", "travel_places", "activities", "objects", "symbols", "flags"]}
+    />
+  );
+};
--- app/(main)/guestbook/_components/EmojiSection.tsx
+++ app/(main)/guestbook/_components/EmojiSection.tsx
@@ -1,27 +1,22 @@
 "use client";
 
-import { useState } from "react";
-// import EmojiPicker from "./EmojiPicker";
+import EmojiPicker from "./EmojiPicker";
 
-export default ({ isMutating, postEntryFunc }: { isMutating: boolean; postEntryFunc: any }) => {
-  const [load, setLoad] = useState(false);
+export default ({ isMutating, postEntryFunc }: { isMutating: boolean; postEntryFunc: any }) => (
+  <>
+    <span className="font-semibold">Send an emote or sign in to send a message:</span>
 
-  return (
-    <>
-      <span className="font-semibold">Send an emote or sign in to send a message:</span>
+    <div className="flex flex-wrap justify-center gap-1">
+      {["😀", "😐", "😥", "😂", "😎", "😍", "🦝"].map(emote => (
+        <button key={emote} className="rounded border border-black p-1 text-xl transition-all hover:bg-black disabled:cursor-not-allowed disabled:bg-gray-300" title={`Send ${emote} anonymously.`} disabled={isMutating} onClick={() => postEntryFunc(emote)}>
+          {emote}
+        </button>
+      ))}
+    </div>
 
-      <div className="flex flex-wrap justify-center gap-1">
-        {["😀", "😐", "😥", "😂", "😎", "😍", "🦝"].map(emote => (
-          <button key={emote} className="rounded border border-black p-1 text-xl transition-all hover:bg-black disabled:cursor-not-allowed disabled:bg-gray-300" title={`Send ${emote} anonymously.`} disabled={isMutating} onClick={() => postEntryFunc(emote)}>
-            {emote}
-          </button>
-        ))}
-      </div>
-
-      <details className="mt-1" onClick={() => setLoad(true)}>
-        <summary className="cursor-pointer">Full Emoji Picker</summary>
-        {/* {load && <EmojiPicker isMutating={isMutating} postEntryFunc={postEntryFunc} />} */}
-      </details>
-    </>
-  );
-};
+    <details className="mt-1">
+      <summary className="cursor-pointer">Full Emoji Picker</summary>
+      <EmojiPicker isMutating={isMutating} postEntryFunc={postEntryFunc} />
+    </details>
+  </>
+);
