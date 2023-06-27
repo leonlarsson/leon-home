6bccf08d7381f06e089e6a6c273a4816a3781b62
Leon San José Larsson <leonlarsson8@gmail.com>
2023-06-27 22:32:42 +0200

    project: update dark mode <details>

--- app/(main)/projects/[id]/page.tsx
+++ app/(main)/projects/[id]/page.tsx
@@ -89,7 +89,7 @@ export default ({ params, searchParams }: { params: { id: string }; searchParams
             ))}
           </div>
 
-          <details className="dark m-auto my-5 rounded border border-black p-2 transition-colors open:bg-black open:text-white hover:bg-black hover:text-white dark:border-kinda-white/50 max-xl:min-w-[70vw] lg:w-[80vw] 2xl:w-[1300px]">
+          <details className="m-auto my-5 rounded border border-black p-2 transition-colors open:bg-black open:text-white hover:bg-black hover:text-white dark:border-kinda-white/50 dark:open:bg-inherit dark:hover:border-kinda-white dark:hover:bg-inherit max-xl:min-w-[70vw] lg:w-[80vw] 2xl:w-[1300px]">
             <summary className="cursor-pointer text-lg font-semibold">Preview {project.projectId === 4 && "(Inception style)"}</summary>
             <iframe src={project.link} className="h-[500px] w-full rounded bg-white lg:h-[500px] xl:h-[700px]"></iframe>
           </details>
