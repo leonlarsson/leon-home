d4429eb2b4b39a87fb52ab6d929484cfbf023c6b
Leon San José Larsson <leonlarsson8@gmail.com>
2023-07-01 01:50:22 +0200

    add config redirect idea

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
   experimental: {
     serverActions: true
   }
+  // redirects: () => {
+  //   return projects.map(project => ({
+  //     source: `/projects/${project.projectId}/go`,
+  //     destination: project.link
+  //   }));
+  // }
 };
 
 module.exports = nextConfig;
