0a1152df24b34ca40e643ce0b651a186eef82587
Leon San José Larsson <leonlarsson8@gmail.com>
2023-07-02 18:26:09 +0200

    guestbook: merge emoji components

--- app/(main)/guestbook/_components/EmojiSection.tsx
+++ app/(main)/guestbook/_components/EmojiSection.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import EmojiPicker from "./EmojiPicker";
+import dynamic from "next/dynamic";
+import { EmojiStyle, Theme } from "emoji-picker-react";
+const EmojiPicker = dynamic(() => import("emoji-picker-react"), { ssr: false });
 
 export default ({ isMutating, postEntryFunc }: { isMutating: boolean; postEntryFunc: any }) => (
   <>
@@ -16,7 +18,26 @@ export default ({ isMutating, postEntryFunc }: { isMutating: boolean; postEntryF
 
     <details className="mt-1">
       <summary className="cursor-pointer">Full Emoji Picker</summary>
-      <EmojiPicker isMutating={isMutating} postEntryFunc={postEntryFunc} />
+
+      {/* npm i @emoji-mart/react
+      <Picker theme="light" dynamicWidth={true} skinTonePosition="none" categories={["people", "nature", "foods", "activity", "places", "objects", "symbols", "flags"]} onEmojiSelect={(e: { native: string }) => !isMutating && postEntryFunc(e.native)} />
+      em-emoji-picker {
+        height: 400px;
+        width: 100%;
+      } */}
+      <EmojiPicker
+        onEmojiClick={e => !isMutating && postEntryFunc(e.emoji)}
+        theme={Theme.AUTO}
+        lazyLoadEmojis
+        skinTonesDisabled
+        emojiStyle={EmojiStyle.NATIVE}
+        emojiVersion="5.0"
+        previewConfig={{ defaultEmoji: "1f47d" }}
+        height={400}
+        width={"100%"}
+        // @ts-expect-error
+        categories={["smileys_people", "animals_nature", "food_drink", "travel_places", "activities", "objects", "symbols", "flags"]}
+      />
     </details>
   </>
 );
