67c90adb55d53f42ae369994d3ecf581cca90774
Leon San José Larsson <leonlarsson8@gmail.com>
2023-07-02 18:58:25 +0200

    guestbook: move TextForm to SendMessageSection
    Also move to using the action prop.
    Removes onFormSubmit.

--- app/(main)/guestbook/_components/SendMessageSection.tsx
+++ app/(main)/guestbook/_components/SendMessageSection.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { useState, useTransition } from "react";
+import { useRef, useState, useTransition } from "react";
 import { useRouter } from "next/navigation";
 import EmojiSection from "./EmojiSection";
-import TextForm from "./TextForm";
 import { postEntry } from "../apiActions";
 
 export default ({ mode, name }: { mode: "text" | "emoji"; name?: string }) => {
@@ -13,39 +12,42 @@ export default ({ mode, name }: { mode: "text" | "emoji"; name?: string }) => {
   const [isError, setIsError] = useState(false);
   const isMutating = isFetching || isPending;
 
-  // On form submit, grab the info and send to postEntryFunc, which then sends the actual request
-  const onFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setIsFetching(true);
-    setIsError(false);
-
-    const form = e.currentTarget;
-    const input = form.elements.namedItem("message") as HTMLInputElement;
-
-    const postWasOk = await postEntryFunc(input.value);
-    if (postWasOk) input.value = "";
-  };
-
   // Send the message to the API
   const postEntryFunc = async (message: string): Promise<boolean> => {
-    // Post message (server action)
+    setIsFetching(true);
+    setIsError(false);
+    // SERVER ACTION
     const postWasOk = await postEntry(message, name);
-
     setIsFetching(false);
 
-    if (postWasOk) {
-      startTransition(() => router.refresh());
-    } else {
-      setIsError(true);
-    }
-
+    postWasOk ? startTransition(() => router.refresh()) : setIsError(true);
     return postWasOk;
   };
 
   return (
     <div>
-      {mode === "emoji" ? <EmojiSection isMutating={isMutating} postEntryFunc={postEntryFunc} /> : <TextForm isPending={isPending} isMutating={isMutating} onFormSubmit={onFormSubmit} />}
+      {mode === "emoji" ? <EmojiSection isMutating={isMutating} postEntryFunc={postEntryFunc} /> : <TextForm isPending={isPending} isMutating={isMutating} postEntryFunc={postEntryFunc} />}
       {isError && <span className="mt-1 text-red-500 dark:text-red-400">Failed to send message.</span>}
     </div>
+  );
+};
+
+const TextForm = ({ isPending, isMutating, postEntryFunc }: { isPending: boolean; isMutating: boolean; postEntryFunc: (message: string) => Promise<boolean> }) => {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  return (
+    <form
+      className="flex justify-center gap-2 max-[370px]:flex-col"
+      ref={formRef}
+      action={async formData => {
+        const postWasOk = await postEntryFunc(formData.get("message") as string);
+        if (postWasOk) formRef.current?.reset();
+      }}
+    >
+      <input className="text-input disabled:cursor-not-allowed disabled:bg-neutral-300 dark:disabled:bg-neutral-600" type="text" name="message" placeholder="Your message..." required disabled={isPending} maxLength={50} />
+      <button className="button-with-border disabled:cursor-not-allowed disabled:bg-neutral-300 dark:disabled:bg-neutral-600" type="submit" disabled={isMutating}>
+        <i className="fa-solid fa-paper-plane" /> Send
+      </button>
+    </form>
   );
 };
