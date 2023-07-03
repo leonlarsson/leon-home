ef9518c0018ad5cd808ca28c0184d2cbf01856d4
Leon San José Larsson <leonlarsson8@gmail.com>
2023-07-03 21:42:24 +0200

    guestbook: improve look of emoji picker

--- app/(main)/guestbook/_components/EmojiSection.tsx
+++ app/(main)/guestbook/_components/EmojiSection.tsx
@@ -8,7 +8,7 @@ export default ({ isMutating, postEntryFunc }: { isMutating: boolean; postEntryF
 
     <div className="flex flex-wrap justify-center gap-1">
       {emojis.map(emote => (
-        <button key={emote} className="button-with-border !p-1 text-xl disabled:cursor-not-allowed disabled:bg-gray-300" title={`Send ${emote} anonymously.`} disabled={isMutating} onClick={() => postEntryFunc(emote)}>
+        <button key={emote} className="button-with-border h-10 w-10 !p-0 text-xl disabled:cursor-not-allowed disabled:bg-gray-300" title={`Send ${emote} anonymously.`} disabled={isMutating} onClick={() => postEntryFunc(emote)}>
           {emote}
         </button>
       ))}
