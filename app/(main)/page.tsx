41bf26c0787747e44aa8a2e35853c19f219258eb
Leon San José Larsson <leonlarsson8@gmail.com>
2023-07-20 00:42:12 +0200

    home: adjust link padding and spacing
    Add animation to separation border.

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -9,7 +9,7 @@ export default () => {
       <span className="text-[2rem] font-extrabold leading-none max-[430px]:text-[1.5rem]">Leon San José Larsson</span>
       <Employment title="Community & Support Specialist" companyName="Sharkmob" companyUrl="https://sharkmob.com" />
 
-      <div className="mb-1 mt-2 flex flex-wrap justify-center gap-x-1 border-b-slate-900 dark:border-kinda-white max-[651px]:border-b-4">
+      <div className="mb-1 mt-2 flex flex-wrap justify-center gap-x-[2px] transition-all border-b-slate-900 dark:border-kinda-white max-[665px]:border-b-4">
         <ProfileLink title="Email" url="mailto:leonlarsson8@gmail.com" icon={<i className="fa-solid fa-envelope" />} newTab />
         <ProfileLink title="LinkedIn" url="https://www.linkedin.com/in/leonlarsson/" icon={<i className="fa-brands fa-linkedin" />} newTab />
         <ProfileLink title="Twitter" url="https://twitter.com/MozzyFX" icon={<i className="fa-brands fa-twitter" />} newTab />
@@ -21,7 +21,7 @@ export default () => {
       {/* Used for creating embed images for 900x600 */}
       {/* <div className="absolute left-[15px] top-[565px] text-lg">leonlarsson.com</div> */}
 
-      <div className="flex flex-wrap justify-center gap-x-1">
+      <div className="flex flex-wrap justify-center gap-x-[2px]">
         <ProfileLink title="Projects" url="/projects" icon={<i className="fa-solid fa-arrow-right transition-transform group-hover:translate-x-[3px]" />} />
         <ProfileLink title="Guestbook" url="/guestbook" icon={<i className="fa-solid fa-book transition-transform group-hover:-rotate-12" />} />
         <ProfileLink title="English CV" url="/cv" icon={<i className="fa-solid fa-file-text" />} />
@@ -49,7 +49,7 @@ const Employment = ({ title, companyName, companyUrl }: { title: string; company
 };
 
 const ProfileLink = ({ title, url, icon, newTab }: { title: string; url: string; icon?: React.ReactNode; newTab?: boolean }) => (
-  <Link className="rounded p-1 text-lg font-semibold transition-all hover:bg-black hover:text-white dark:hover:bg-gray-300/10 dark:hover:text-kinda-white group" href={url} target={newTab ? "_blank" : "_self"} draggable={false}>
+  <Link className="rounded p-[6px] leading-tight text-lg font-semibold transition-all hover:bg-black hover:text-white dark:hover:bg-gray-300/10 dark:hover:text-kinda-white group" href={url} target={newTab ? "_blank" : "_self"} draggable={false}>
     {icon} {title}
   </Link>
 );
