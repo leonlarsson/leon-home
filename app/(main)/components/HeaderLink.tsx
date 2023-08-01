c2a365fbb2e2e2d0459da60f68e56959cba2335f
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-01 14:10:14 +0200

    Header: add arrow to link when inside a project
    Also adjusts Header breakpoints.

--- app/(main)/components/HeaderLink.tsx
+++ app/(main)/components/HeaderLink.tsx
@@ -4,10 +4,11 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default ({ content, url, title, newTab = false }: { content: string | React.ReactNode; url: string; title?: string; newTab?: boolean }) => {
-  const activeLink = usePathname() === url || (usePathname().startsWith("/projects") && url.startsWith("/projects"));
+  const inProject = usePathname() === url || (usePathname().startsWith("/projects") && url.startsWith("/projects"));
 
   return (
-    <Link className={`py-4 text-neutral-800 transition-all hover:text-black dark:text-neutral-400 dark:hover:text-kinda-white ${activeLink ? "font-bold !text-black dark:!text-kinda-white" : ""}`} href={url} target={newTab ? "_blank" : "_self"} title={title} draggable={false}>
+    <Link className={`py-4 text-neutral-800 transition-all hover:text-black dark:text-neutral-400 dark:hover:text-kinda-white ${inProject ? "font-bold !text-black dark:!text-kinda-white" : ""}`} href={url} target={newTab ? "_blank" : "_self"} title={title} draggable={false}>
+      {inProject && usePathname().split("/")[2] && <i className="fa-solid fa-arrow-left fa-sm me-1" />}
       {content}
     </Link>
   );
