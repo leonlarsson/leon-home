b512b2aa8fb16117890a5f97d013ea3af14f5b23
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-01 17:40:14 +0200

    home: completely redesign the home page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,33 +1,92 @@
 import Image from "next/image";
 import Link from "next/link";
+import projects from "./projects/data";
+import smLogo from "/public/assets/images/smlogo_notext.png";
+import { Project } from "./projects/components/ProjectsGrid";
 
 export default () => {
   return (
-    <div className="m-auto">
-      <div className="flex flex-col">
-        <Image className="z-0 mb-2 self-center rounded-full shadow-xl transition-all hover:rounded-b-xl hover:shadow-2xl active:-translate-y-1 max-[430px]:w-2/5" src="/assets/images/avatar.png" alt="Leon image" width={150} height={150} priority draggable={false} />
+    <div className="mx-auto max-w-3xl pb-10 text-start">
+      <div className="mb-6 flex items-center gap-4">
+        {/* <div className="rounded-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1 hover:from-red-500 hover:via-lime-600 hover:to-blue-400 max-md:p-px">IMAGE</div> */}
+        <Image className="h-full rounded-full shadow-xl transition-all hover:rounded-l-xl hover:shadow-2xl active:-translate-y-1 max-md:w-[50px]" src="/assets/images/avatar.png" alt="Leon image" width={130} height={130} priority draggable={false} />
 
-        <span className="z-30 text-[2rem] font-extrabold leading-none max-[430px]:text-[1.5rem]">Leon San José Larsson</span>
-        <Employment title="Community & Support Specialist" companyName="Sharkmob" companyUrl="https://sharkmob.com" />
+        <div className="flex flex-col gap-1 max-md:gap-0">
+          <span className="text-4xl font-extrabold leading-none transition-all max-md:text-xl">Leon San José Larsson</span>
+          <Employment title="Community & Support Specialist" companyName="Sharkmob" companyUrl="https://sharkmob.com" />
+        </div>
       </div>
 
-      <div className="mb-1 mt-2 flex flex-wrap justify-center gap-x-[2px] border-b-slate-900 transition-all dark:border-kinda-white max-[665px]:border-b-4">
-        <ProfileLink title="Email" url="mailto:leonlarsson8@gmail.com" icon={<i className="fa-solid fa-envelope" />} newTab />
-        <ProfileLink title="LinkedIn" url="https://www.linkedin.com/in/leonlarsson/" icon={<i className="fa-brands fa-linkedin" />} newTab />
-        <ProfileLink title="Twitter" url="https://x.com/MozzyFX" icon={<i className="fa-brands fa-twitter" />} newTab />
-        <ProfileLink title="Instagram" url="https://www.instagram.com/leonsjlarsson/" icon={<i className="fa-brands fa-instagram" />} newTab />
-        <ProfileLink title="GitHub" url="https://github.com/leonlarsson" icon={<i className="fa-brands fa-github" />} newTab />
-        <ProfileLink title="ArtStation" url="https://www.artstation.com/leonlarsson" icon={<i className="fa-brands fa-artstation" />} newTab />
-      </div>
+      <div className="flex flex-col gap-6">
+        <div>
+          Hello, I am <span className="font-bold">Leon San José Larsson.</span>
+        </div>
 
-      {/* Used for creating embed images for 900x600 */}
-      {/* <div className="absolute left-[15px] top-[565px] text-lg">leonlarsson.com</div> */}
+        {/* INTRO */}
+        <div>
+          I'm a hobby web developer in self-training.
+          <div>
+            I currently work as the <b>Community & Support Specialist</b> at{" "}
+            <Link href="https://sharkmob.com" target="_blank" className="inline-flex items-baseline underline-offset-2 hover:underline" draggable={false}>
+              <Image src={smLogo} alt="Sharkmob logo" width={16} height={16} className="mx-1 self-center" />
+              <span>Sharkmob</span>
+            </Link>
+            , where I manage the player support, Bloodhunt's website, and help the community team.
+          </div>
+        </div>
 
-      <div className="flex flex-wrap justify-center gap-x-[2px]">
-        <ProfileLink title="Projects" url="/projects" icon={<i className="fa-solid fa-arrow-right transition-transform group-hover:translate-x-[3px]" />} />
-        <ProfileLink title="Guestbook" url="/guestbook" icon={<i className="fa-solid fa-book transition-transform group-hover:-rotate-12" />} />
-        <ProfileLink title="English CV" url="/cv" icon={<i className="fa-solid fa-file-text" />} />
-        <ProfileLink title="Swedish CV" url="/cv-swe" icon={<i className="fa-solid fa-file-text" />} />
+        {/* CONNECT */}
+        <div className="flex flex-col gap-1">
+          <span className="text-xl font-semibold">Connect with me</span>
+          <div className="grid grid-cols-1 gap-2 min-[370px]:grid-cols-2 sm:grid-cols-3">
+            {[
+              { title: "Email", url: "mailto:leonlarsson8@gmail.com", iconClasses: "fa-solid fa-envelope" },
+              { title: "LinkedIn", url: "https://www.linkedin.com/in/leonlarsson/", iconClasses: "fa-brands fa-linkedin" },
+              { title: "X / Twitter", url: "https://x.com/MozzyFX", iconClasses: "fa-brands fa-twitter" },
+              { title: "Instagram", url: "https://www.instagram.com/leonsjlarsson/", iconClasses: "fa-brands fa-instagram" },
+              { title: "GitHub", url: "https://github.com/leonlarsson", iconClasses: "fa-brands fa-github" },
+              { title: "ArtStation", url: "https://www.artstation.com/leonlarsson", iconClasses: "fa-brands fa-artstation" }
+            ].map(({ title, url, iconClasses }) => (
+              <div className="rounded bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-[2px] hover:from-red-500 hover:via-lime-600 hover:to-blue-400">
+                <Link href={url} target="_blank" className="flex items-center justify-between rounded bg-gradient-to-bl from-white to-slate-200 p-2 dark:from-kinda-black dark:to-kinda-black">
+                  <div>
+                    <i className={`${iconClasses} fa-lg fa-fw me-1`} />
+                    <span>{title}</span>
+                  </div>
+                  <i className="fa-solid fa-external-link dark:text-kinda-white" />
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* RESUME */}
+        <div className="flex flex-col gap-1">
+          <span className="mt-2 text-xl font-semibold">Want to learn more?</span>
+          <div className="rounded bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-[2px] hover:from-red-500 hover:via-lime-600 hover:to-blue-400">
+            <Link href="/resume" className="flex h-full rounded bg-gradient-to-bl from-white to-slate-200 p-2 dark:from-kinda-black dark:to-kinda-black">
+              <span>
+                <i className="fa-solid fa-file-text fa-fw me-1" />
+                Read my Resume
+              </span>
+            </Link>
+          </div>
+        </div>
+
+        {/* FEATURED PROJECTS */}
+        <div className="flex flex-col gap-1">
+          <span className="mt-2 text-xl font-semibold">
+            Featured{" "}
+            <Link href="/projects" className="underline-offset-2 hover:underline">
+              projects
+            </Link>
+          </span>
+          <div className="flex flex-col gap-2">
+            <Project project={projects[17] ?? projects[17]} />
+            <Project project={projects[0]} />
+            <Project project={projects[7]} />
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -35,7 +94,7 @@ export default () => {
 
 const Employment = ({ title, companyName, companyUrl }: { title: string; companyName?: string; companyUrl?: string }) => {
   return (
-    <span className="text-lg max-[430px]:text-sm">
+    <span className="text-xl transition-all max-md:text-sm">
       {title}
       {companyName && (
         <>
@@ -49,9 +108,3 @@ const Employment = ({ title, companyName, companyUrl }: { title: string; company
     </span>
   );
 };
-
-const ProfileLink = ({ title, url, icon, newTab }: { title: string; url: string; icon?: React.ReactNode; newTab?: boolean }) => (
-  <Link className="group rounded p-[6px] text-lg font-semibold leading-tight transition-all hover:bg-black hover:text-white dark:hover:bg-gray-300/10 dark:hover:text-kinda-white" href={url} target={newTab ? "_blank" : "_self"} draggable={false}>
-    {icon} {title}
-  </Link>
-);
