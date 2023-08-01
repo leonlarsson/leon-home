91d03eb45176b0d272bc43d063726d7800e9bc96
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-01 14:27:56 +0200

    Header: change padding/margin

--- app/(main)/components/Header.tsx
+++ app/(main)/components/Header.tsx
@@ -6,7 +6,7 @@ export default () => {
   return (
     <>
       {/* TOP HEADER */}
-      <header className="relative z-50 mb-8 hidden px-4 min-[610px]:block">
+      <header className="relative z-50 mb-6 hidden px-6 min-[610px]:block">
         <div className="container mx-auto mt-8 flex h-16 w-full max-w-5xl justify-between rounded-xl border-[1px] border-black/20 bg-white/50 px-4 backdrop-blur-md dark:border-kinda-white/5 dark:bg-[#1e1e1e]/50">
           <div className="flex items-center gap-6">
             <Link className="self-center" href="/" draggable={false}>
@@ -29,7 +29,7 @@ export default () => {
       </header>
 
       {/* BOTTOM OVERLAY HEADER */}
-      <header className="fixed inset-x-0 bottom-0 z-50 block px-4 pb-6 min-[610px]:hidden" style={{ opacity: 1, transform: "none" }}>
+      <header className="fixed inset-x-0 bottom-0 z-50 block px-6 pb-6 min-[610px]:hidden" style={{ opacity: 1, transform: "none" }}>
         <div className="flex h-16 w-full justify-between rounded-xl border-[1px] border-black/20 bg-white/30 px-4 text-xl backdrop-blur-md dark:border-kinda-white/5 dark:bg-[#1e1e1e]/30">
           <div className="flex w-full items-center justify-around gap-6 min-[450px]:justify-normal">
             <HeaderLink content={<i className="fa-solid fa-house" />} url="/" title="Home" />
