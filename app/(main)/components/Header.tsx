2a66e05d45f7723512e1a8be723ffdab7dcac50d
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-01 17:57:39 +0200

    Header: remove Instagram link
    Also adjusts breakpoints.

--- app/(main)/components/Header.tsx
+++ app/(main)/components/Header.tsx
@@ -6,7 +6,7 @@ export default () => {
   return (
     <>
       {/* TOP HEADER */}
-      <header className="relative z-50 mb-6 hidden px-6 min-[610px]:block">
+      <header className="relative z-50 mb-6 hidden px-6 min-[490px]:block">
         <div className="container mx-auto mt-8 flex h-16 w-full max-w-5xl justify-between rounded-xl border-[1px] border-black/20 bg-white/50 px-4 backdrop-blur-md dark:border-kinda-white/5 dark:bg-[#1e1e1e]/50">
           <div className="flex items-center gap-6">
             <Link className="self-center" href="/" draggable={false}>
@@ -18,31 +18,29 @@ export default () => {
             <HeaderLink content="Resume" url="/resume" />
           </div>
 
-          <div className="hidden items-center gap-6 text-2xl min-[720px]:flex">
+          <div className="hidden items-center gap-6 text-2xl min-[670px]:flex">
             <HeaderLink content={<i className="fa-solid fa-envelope" />} url="mailto:leonlarsson8@gmail.com" title="Email" newTab />
             <HeaderLink content={<i className="fa-brands fa-linkedin" />} url="https://www.linkedin.com/in/leonlarsson/" title="LinkedIn" newTab />
             <HeaderLink content={<i className="fa-brands fa-twitter" />} url="https://x.com/MozzyFX" title="X (Twitter)" newTab />
-            <HeaderLink content={<i className="fa-brands fa-instagram" />} url="https://www.instagram.com/leonsjlarsson/" title="Instagram" newTab />
             <HeaderLink content={<i className="fa-brands fa-github" />} url="https://github.com/leonlarsson" title="GitHub" newTab />
           </div>
         </div>
       </header>
 
       {/* BOTTOM OVERLAY HEADER */}
-      <header className="fixed inset-x-0 bottom-0 z-50 block px-6 pb-6 min-[610px]:hidden" style={{ opacity: 1, transform: "none" }}>
+      <header className="fixed inset-x-0 bottom-0 z-50 block px-6 pb-6 min-[490px]:hidden" style={{ opacity: 1, transform: "none" }}>
         <div className="flex h-16 w-full justify-between rounded-xl border-[1px] border-black/20 bg-white/30 px-4 text-xl backdrop-blur-md dark:border-kinda-white/5 dark:bg-[#1e1e1e]/30">
-          <div className="flex w-full items-center justify-around gap-6 min-[450px]:justify-normal">
+          <div className="flex w-full items-center justify-around gap-6 min-[410px]:justify-normal">
             <HeaderLink content={<i className="fa-solid fa-house" />} url="/" title="Home" />
             <HeaderLink content={<i className="fa-solid fa-tools" />} url="/projects" title="Projects" />
             <HeaderLink content={<i className="fa-solid fa-book" />} url="/guestbook" title="Guestbook" />
             <HeaderLink content={<i className="fa-solid fa-file-text" />} url="/resume" title="CV/Resume" />
           </div>
 
-          <div className="hidden items-center gap-6 min-[450px]:flex">
+          <div className="hidden items-center gap-6 min-[410px]:flex">
             <HeaderLink content={<i className="fa-solid fa-envelope" />} url="mailto:leonlarsson8@gmail.com" title="Email" newTab />
             <HeaderLink content={<i className="fa-brands fa-linkedin" />} url="https://www.linkedin.com/in/leonlarsson/" title="LinkedIn" newTab />
             <HeaderLink content={<i className="fa-brands fa-twitter" />} url="https://x.com/MozzyFX" title="X (Twitter)" newTab />
-            <HeaderLink content={<i className="fa-brands fa-instagram" />} url="https://www.instagram.com/leonsjlarsson/" title="Instagram" newTab />
             <HeaderLink content={<i className="fa-brands fa-github" />} url="https://github.com/leonlarsson" title="GitHub" newTab />
           </div>
         </div>
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -35,8 +35,8 @@ export default ({ children }: { children: React.ReactNode }) => {
     <html className="min-h-[100svh] overflow-y-scroll bg-gradient-to-bl from-white to-slate-200 dark:from-kinda-black dark:to-kinda-black" lang="en">
       <body className={`${inter.className} select-none text-black dark:text-kinda-white`}>
         <Header />
-        {/* Below 610px width, add bottom margin to accomodate the bottom overlay header. Add relative if issues occur */}
-        <div className="container mx-auto p-4 text-center transition-all max-[610px]:mb-24">{children}</div>
+        {/* Below 490px width, add bottom margin to accomodate the bottom overlay header. Add relative if issues occur */}
+        <div className="container mx-auto p-4 text-center transition-all max-[490px]:mb-24">{children}</div>
       </body>
     </html>
   );
