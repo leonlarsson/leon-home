f88d787d59933781e8f22f9d6583b3fc83f04245
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-02 12:49:26 +0200

    Header: adjust gaps

--- app/(main)/components/Header.tsx
+++ app/(main)/components/Header.tsx
@@ -11,7 +11,7 @@ export default () => {
           <div className="container mx-auto flex h-16 w-full max-w-5xl items-center justify-between rounded-xl bg-white/50 px-4 backdrop-blur-md dark:bg-[#1e1e1e]/50">REST HERE</div>
         </GradientBorder> */}
         <div className="container mx-auto mt-8 flex h-16 w-full max-w-5xl items-center justify-between rounded-xl border-[1px] border-black/20 bg-white/50 px-4 backdrop-blur-md dark:border-kinda-white/5 dark:bg-[#1e1e1e]/50">
-          <div className="flex gap-6">
+          <div className="flex gap-5">
             <Link className="self-center" href="/" draggable={false}>
               <Image className="rounded-full" src="/assets/images/avatar.png" alt="Leon image" width={40} height={40} priority draggable={false} />
             </Link>
@@ -21,7 +21,7 @@ export default () => {
             <HeaderLink content="Resume" url="/resume" />
           </div>
 
-          <div className="hidden gap-6 text-2xl min-[670px]:flex">
+          <div className="hidden gap-5 text-2xl min-[670px]:flex">
             <HeaderLink content={<i className="fa-solid fa-envelope" />} url="mailto:leonlarsson8@gmail.com" title="Email" newTab />
             <HeaderLink content={<i className="fa-brands fa-linkedin" />} url="https://www.linkedin.com/in/leonlarsson/" title="LinkedIn" newTab />
             <HeaderLink content={<i className="fa-brands fa-twitter" />} url="https://x.com/MozzyFX" title="X (Twitter)" newTab />
@@ -33,14 +33,14 @@ export default () => {
       {/* BOTTOM OVERLAY HEADER */}
       <header className="fixed inset-x-0 bottom-0 z-50 block px-6 pb-6 min-[490px]:hidden" style={{ opacity: 1, transform: "none" }}>
         <div className="flex h-16 w-full items-center justify-between rounded-xl border-[1px] border-black/20 bg-white/30 px-4 text-xl backdrop-blur-md dark:border-kinda-white/5 dark:bg-[#1e1e1e]/30">
-          <div className="flex w-full justify-around gap-6 min-[410px]:justify-normal">
+          <div className="flex w-full justify-around gap-5 min-[410px]:justify-normal">
             <HeaderLink content={<i className="fa-solid fa-house" />} url="/" title="Home" />
             <HeaderLink content={<i className="fa-solid fa-tools" />} url="/projects" title="Projects" />
             <HeaderLink content={<i className="fa-solid fa-book" />} url="/guestbook" title="Guestbook" />
             <HeaderLink content={<i className="fa-solid fa-file-text" />} url="/resume" title="CV/Resume" />
           </div>
 
-          <div className="hidden gap-6 min-[410px]:flex">
+          <div className="hidden gap-5 min-[410px]:flex">
             <HeaderLink content={<i className="fa-solid fa-envelope" />} url="mailto:leonlarsson8@gmail.com" title="Email" newTab />
             <HeaderLink content={<i className="fa-brands fa-linkedin" />} url="https://www.linkedin.com/in/leonlarsson/" title="LinkedIn" newTab />
             <HeaderLink content={<i className="fa-brands fa-twitter" />} url="https://x.com/MozzyFX" title="X (Twitter)" newTab />
