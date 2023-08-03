06de0709817c33cefa209643a01630cecd479cfe
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-03 20:44:01 +0200

    Header: improve clickability

--- app/(main)/components/Header.tsx
+++ app/(main)/components/Header.tsx
@@ -8,11 +8,11 @@ export default () => {
       {/* TOP HEADER */}
       <header className="relative z-50 mb-6 hidden px-6 min-[490px]:block">
         {/* <GradientBorder rounded="rounded-xl" extraClasses="max-w-5xl mx-auto mt-8" hoverable>
-          <div className="container mx-auto flex h-16 w-full max-w-5xl items-center justify-between rounded-xl bg-white/50 px-4 backdrop-blur-md dark:bg-[#1e1e1e]/50">REST HERE</div>
+          <div className="container mx-auto flex h-16 w-full max-w-5xl items-center justify-between rounded-xl bg-white/50 px-2 backdrop-blur-md dark:bg-[#1e1e1e]/50">REST HERE</div>
         </GradientBorder> */}
-        <div className="container mx-auto mt-8 flex h-16 w-full max-w-5xl items-center justify-between rounded-xl border-[1px] border-black/20 bg-white/50 px-4 backdrop-blur-md dark:border-kinda-white/5 dark:bg-[#1e1e1e]/50">
-          <div className="flex gap-5">
-            <Link className="self-center" href="/" draggable={false}>
+        <div className="container mx-auto mt-8 flex h-16 w-full max-w-5xl items-center justify-between rounded-xl border-[1px] border-black/20 bg-white/50 px-2 backdrop-blur-md dark:border-kinda-white/5 dark:bg-[#1e1e1e]/50">
+          <div className="flex gap-1">
+            <Link className="self-center px-2" href="/" draggable={false}>
               <Image className="rounded-full" src="/assets/images/avatar.png" alt="Leon image" width={40} height={40} priority draggable={false} />
             </Link>
             <HeaderLink content="Home" url="/" />
@@ -21,7 +21,7 @@ export default () => {
             <HeaderLink content="Resume" url="/resume" />
           </div>
 
-          <div className="hidden gap-5 text-2xl min-[670px]:flex">
+          <div className="hidden gap-1 text-2xl min-[670px]:flex">
             <HeaderLink content={<i className="fa-solid fa-envelope" />} url="mailto:leonlarsson8@gmail.com" title="Email" newTab />
             <HeaderLink content={<i className="fa-brands fa-linkedin" />} url="https://www.linkedin.com/in/leonlarsson/" title="LinkedIn" newTab />
             <HeaderLink content={<i className="fa-brands fa-twitter" />} url="https://x.com/MozzyFX" title="X / Twitter" newTab />
--- app/(main)/components/HeaderLink.tsx
+++ app/(main)/components/HeaderLink.tsx
@@ -7,7 +7,7 @@ export default ({ content, url, title, newTab = false }: { content: string | Rea
   const inProject = usePathname() === url || (usePathname().startsWith("/projects") && url.startsWith("/projects"));
 
   return (
-    <Link className={`py-4 text-neutral-800 transition-all hover:text-black dark:text-neutral-400 dark:hover:text-kinda-white ${inProject ? "font-bold !text-black dark:!text-kinda-white" : ""}`} href={url} target={newTab ? "_blank" : "_self"} title={title} draggable={false}>
+    <Link className={`px-2 py-4 text-neutral-800 transition-all hover:text-black dark:text-neutral-400 dark:hover:text-kinda-white ${inProject ? "font-bold !text-black dark:!text-kinda-white" : ""}`} href={url} target={newTab ? "_blank" : "_self"} title={title} draggable={false}>
       {inProject && usePathname().split("/")[2] && <i className="fa-solid fa-arrow-left fa-sm me-1" />}
       {content}
     </Link>
