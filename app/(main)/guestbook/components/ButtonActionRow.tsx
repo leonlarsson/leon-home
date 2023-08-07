8a705d4bbecbee80fd77345a0d712fff5eacedd4
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-08 00:38:50 +0200

    ButtonActionRow: use <dialog> for deleting

--- app/(main)/guestbook/components/ButtonActionRow.tsx
+++ app/(main)/guestbook/components/ButtonActionRow.tsx
@@ -7,7 +7,8 @@ import { Entry } from "@/types";
 
 export default ({ entry }: { entry: Entry }) => {
   const router = useRouter();
-  const dialogRef = useRef<HTMLDialogElement>(null);
+  const editDialogRef = useRef<HTMLDialogElement>(null);
+  const deleteDialogRef = useRef<HTMLDialogElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
   const onEditModalSaveClick = async () => {
@@ -19,16 +20,15 @@ export default ({ entry }: { entry: Entry }) => {
     if (entryWasEdited) router.refresh();
   };
 
-  const onDeleteClick = async () => {
-    const confirmDelete = confirm(`Are you sure you want to delete entry with ID ${entry.id}?\n\nMessage:\n${entry.body}`);
-    if (!confirmDelete) return;
+  const onDeleteModalConfirmClick = async () => {
     const entryWasDeleted = await deleteEntry(entry.id);
     if (entryWasDeleted) router.refresh();
   };
 
   return (
     <>
-      <EditMessageDialog entry={entry} dialogRef={dialogRef} inputRef={inputRef} saveFunc={onEditModalSaveClick} />
+      <EditMessageDialog entry={entry} editDialogRef={editDialogRef} inputRef={inputRef} saveFunc={onEditModalSaveClick} />
+      <DeleteMessageDialog entry={entry} deleteDialogRef={deleteDialogRef} deleteFunc={onDeleteModalConfirmClick} />
 
       <span className="me-2 inline-flex gap-1 text-base">
         <button
@@ -36,12 +36,12 @@ export default ({ entry }: { entry: Entry }) => {
           title={`Edit entry message with ID ${entry.id}.`}
           onClick={() => {
             if (inputRef?.current) inputRef.current.value = entry.body;
-            dialogRef.current?.showModal();
+            editDialogRef.current?.showModal();
           }}
         >
           <i className="fa-solid fa-pen fa-fw" />
         </button>
-        <button className="button-with-border !px-1 !py-0" title={`Delete entry with ID ${entry.id}.`} onClick={onDeleteClick}>
+        <button className="button-with-border !px-1 !py-0" title={`Delete entry with ID ${entry.id}.`} onClick={() => deleteDialogRef.current?.showModal()}>
           <i className="fa-solid fa-trash fa-fw" />
         </button>
       </span>
@@ -51,12 +51,12 @@ export default ({ entry }: { entry: Entry }) => {
 
 type EditDialogProps = {
   entry: Entry;
-  dialogRef: React.RefObject<HTMLDialogElement>;
+  editDialogRef: React.RefObject<HTMLDialogElement>;
   inputRef: React.RefObject<HTMLInputElement>;
   saveFunc: Function;
 };
 
-const EditMessageDialog = ({ entry, dialogRef, inputRef, saveFunc }: EditDialogProps) => {
+const EditMessageDialog = ({ entry, editDialogRef, inputRef, saveFunc }: EditDialogProps) => {
   return (
     <dialog
       className="mx-auto w-full max-w-lg rounded border bg-kinda-white p-2 backdrop:bg-black/30 backdrop:backdrop-blur-sm dark:border-white/20 dark:bg-kinda-black dark:backdrop:bg-black/50"
@@ -67,7 +67,7 @@ const EditMessageDialog = ({ entry, dialogRef, inputRef, saveFunc }: EditDialogP
         //   dialogRef.current?.close();
         // }
       }}
-      ref={dialogRef}
+      ref={editDialogRef}
     >
       <div className="select-none text-lg font-semibold">Edit message</div>
       <div className="mb-3 select-none">Max 100 characters.</div>
@@ -82,24 +82,66 @@ const EditMessageDialog = ({ entry, dialogRef, inputRef, saveFunc }: EditDialogP
         onKeyDown={e => {
           if (e.key === "Enter") {
             e.preventDefault();
-            dialogRef.current?.close();
+            editDialogRef.current?.close();
             saveFunc();
           }
         }}
       />
 
       <div className="mt-2 flex justify-end gap-2">
-        <button className="button-with-border" onClick={() => dialogRef.current?.close()}>
-          Close
+        <button className="button-with-border" onClick={() => editDialogRef.current?.close()}>
+          Cancel
         </button>
         <button
           className="button-with-border"
           onClick={() => {
-            dialogRef.current?.close();
+            editDialogRef.current?.close();
             saveFunc();
           }}
         >
           Save
+        </button>
+      </div>
+    </dialog>
+  );
+};
+
+type DeleteDialogProps = {
+  entry: Entry;
+  deleteDialogRef: React.RefObject<HTMLDialogElement>;
+  deleteFunc: Function;
+};
+
+const DeleteMessageDialog = ({ entry, deleteDialogRef, deleteFunc }: DeleteDialogProps) => {
+  return (
+    <dialog
+      className="mx-auto max-w-lg rounded border bg-kinda-white p-2 backdrop:bg-black/30 backdrop:backdrop-blur-sm dark:border-white/20 dark:bg-kinda-black dark:backdrop:bg-black/50"
+      onClick={e => {
+        // This code handles clicks outside of the dialog, which closes it
+        // const dialogDimensions = deleteDialogRef.current?.getBoundingClientRect();
+        // if (dialogDimensions && (e.clientX < dialogDimensions.left || e.clientX > dialogDimensions.right || e.clientY < dialogDimensions.top || e.clientY > dialogDimensions.bottom)) {
+        //   deleteDialogRef.current?.close();
+        // }
+      }}
+      ref={deleteDialogRef}
+    >
+      <div className="mb-3 select-none text-lg font-semibold">Delete entry</div>
+      <div className="select-none">
+        <q>{entry.body}</q>
+      </div>
+
+      <div className="mt-2 flex justify-end gap-2">
+        <button className="button-with-border" onClick={() => deleteDialogRef.current?.close()}>
+          Cancel
+        </button>
+        <button
+          className="button-with-border hover:!bg-red-600"
+          onClick={() => {
+            deleteDialogRef.current?.close();
+            deleteFunc();
+          }}
+        >
+          Delete
         </button>
       </div>
     </dialog>
