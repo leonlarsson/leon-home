46bcd949ccf5ed5d313ce8ea6be7893e7a6a7772
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-08 19:59:56 +0200

    guestbook: remove unused transition

--- app/(main)/guestbook/components/EmojiSection.tsx
+++ app/(main)/guestbook/components/EmojiSection.tsx
@@ -2,13 +2,13 @@
 
 import emojis from "../lib/emojis";
 
-export default ({ isMutating, postEntryFunc }: { isMutating: boolean; postEntryFunc: any }) => (
+export default ({ isInserting, postEntryFunc }: { isInserting: boolean; postEntryFunc: any }) => (
   <>
     <span className="font-semibold">Send an emote or sign in to send a message:</span>
 
     <div className="flex flex-wrap justify-center gap-1">
       {emojis.map(emote => (
-        <button key={emote} className="button-with-border h-10 w-10 !p-0 text-xl disabled:cursor-not-allowed disabled:bg-gray-300" title={`Send ${emote} anonymously.`} disabled={isMutating} onClick={() => postEntryFunc(emote)}>
+        <button key={emote} className="button-with-border h-10 w-10 !p-0 text-xl disabled:cursor-not-allowed disabled:bg-gray-300" title={`Send ${emote} anonymously.`} disabled={isInserting} onClick={() => postEntryFunc(emote)}>
           {emote}
         </button>
       ))}
--- app/(main)/guestbook/components/SendMessageSection.tsx
+++ app/(main)/guestbook/components/SendMessageSection.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import { useState, useTransition } from "react";
+import { useState } from "react";
 import EmojiSection from "./EmojiSection";
 import { postEntry } from "../lib/actions";
 
 export default ({ mode }: { mode: "text" | "emoji" }) => {
-  const [isPending, startTransition] = useTransition();
   const [isInserting, setIsInserting] = useState(false);
   const [isError, setIsError] = useState(false);
-  const isMutating = isInserting || isPending;
 
   // Insert entry into database
   const postEntryFunc = async (message: string): Promise<boolean> => {
@@ -23,13 +21,13 @@ export default ({ mode }: { mode: "text" | "emoji" }) => {
 
   return (
     <div>
-      {mode === "emoji" ? <EmojiSection isMutating={isMutating} postEntryFunc={postEntryFunc} /> : <TextForm isMutating={isMutating} postEntryFunc={postEntryFunc} />}
+      {mode === "emoji" ? <EmojiSection isInserting={isInserting} postEntryFunc={postEntryFunc} /> : <TextForm isInserting={isInserting} postEntryFunc={postEntryFunc} />}
       {isError && <span className="mt-1 text-red-500 dark:text-red-400">Failed to send message.</span>}
     </div>
   );
 };
 
-const TextForm = ({ isMutating, postEntryFunc }: { isMutating: boolean; postEntryFunc: (message: string) => Promise<boolean> }) => {
+const TextForm = ({ isInserting, postEntryFunc }: { isInserting: boolean; postEntryFunc: (message: string) => Promise<boolean> }) => {
   return (
     <form
       className="flex justify-center gap-2 max-[370px]:flex-col"
@@ -41,8 +39,8 @@ const TextForm = ({ isMutating, postEntryFunc }: { isMutating: boolean; postEntr
         if (postWasOk) form.reset();
       }}
     >
-      <input className="text-input disabled:cursor-not-allowed disabled:bg-neutral-300 dark:disabled:bg-neutral-600" type="text" name="message" placeholder="Your message..." required disabled={isMutating} maxLength={100} />
-      <button className="button-with-border disabled:cursor-not-allowed disabled:bg-neutral-300 dark:disabled:bg-neutral-600" type="submit" disabled={isMutating}>
+      <input className="text-input disabled:cursor-not-allowed disabled:bg-neutral-300 dark:disabled:bg-neutral-600" type="text" name="message" placeholder="Your message..." required disabled={isInserting} maxLength={100} />
+      <button className="button-with-border disabled:cursor-not-allowed disabled:bg-neutral-300 dark:disabled:bg-neutral-600" type="submit" disabled={isInserting}>
         <i className="fa-solid fa-paper-plane" /> Send
       </button>
     </form>
