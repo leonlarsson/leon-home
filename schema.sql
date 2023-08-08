5c11c8f67482544e5ef5b59a1b39c5b85e996762
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-08 18:16:48 +0200

    journal: kill

--- schema.sql
+++ schema.sql
@@ -16,10 +16,3 @@ CREATE TABLE guestbook_edits (
     new_message VARCHAR(100) NOT NULL,
     edited_by VARCHAR(150) DEFAULT NULL
 );
-
-CREATE TABLE journal_posts (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    title TEXT NOT NULL,
-    body TEXT NOT NULL
-);
