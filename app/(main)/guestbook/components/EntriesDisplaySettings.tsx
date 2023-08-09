58b3c1e354e834d8b2ac0c345264868d32a81a92
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-09 19:49:44 +0200

    guestbook: remove outline-none for checks

--- app/(main)/guestbook/components/EntriesDisplaySettings.tsx
+++ app/(main)/guestbook/components/EntriesDisplaySettings.tsx
@@ -18,14 +18,14 @@ export default () => {
   return (
     <div className="flex flex-wrap">
       <div className="me-3 flex items-center gap-1">
-        <input className="peer h-4 w-4 cursor-pointer accent-black outline-none dark:accent-kinda-white" type="checkbox" id="named" defaultChecked={searchParams.get("named") === "true"} onChange={onNamedEntriesChange} />
+        <input className="peer h-4 w-4 cursor-pointer accent-black dark:accent-kinda-white" type="checkbox" id="named" defaultChecked={searchParams.get("named") === "true"} onChange={onNamedEntriesChange} />
         <label className="select-none underline-offset-2 opacity-75 transition-opacity peer-checked:underline peer-checked:opacity-100 dark:opacity-50" htmlFor="named">
           Show only named entries
         </label>
       </div>
 
       <div className="flex items-center gap-1">
-        <input className="peer h-4 w-4 cursor-pointer accent-black outline-none dark:accent-kinda-white" type="checkbox" id="timestamps" defaultChecked={showTimestamps} onChange={e => setShowTimestamps(e.target.checked)} />
+        <input className="peer h-4 w-4 cursor-pointer accent-black dark:accent-kinda-white" type="checkbox" id="timestamps" defaultChecked={showTimestamps} onChange={e => setShowTimestamps(e.target.checked)} />
         <label className="select-none underline-offset-2 opacity-75 transition-opacity peer-checked:underline peer-checked:opacity-100 dark:opacity-50" htmlFor="timestamps">
           Show timestamps
         </label>
