694db13f15a3d422e52253afdef634457467ac6b
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-17 19:57:50 +0200

    SpotifyCurrentTrack: add paused indicator

--- app/(main)/music/components/SpotifyCurrentTrack.tsx
+++ app/(main)/music/components/SpotifyCurrentTrack.tsx
@@ -44,7 +44,7 @@ export default async ({ compact, alwaysRender, currentlyPlayingText, hideSpotify
 
             {track && (
               <span className="text-sm text-neutral-700 dark:text-neutral-300">
-                {formatDuration(currentlyPlayingObject?.progress_ms ?? 0)} / {formatDuration(track.duration_ms)}
+                {currentlyPlayingObject?.is_playing ? formatDuration(currentlyPlayingObject?.progress_ms ?? 0) : <i className="fa-solid fa-pause" />} / {formatDuration(track.duration_ms)}
               </span>
             )}
           </div>
