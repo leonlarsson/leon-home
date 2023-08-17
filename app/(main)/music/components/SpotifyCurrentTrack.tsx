caf4f5b04451caabd1998f5759331e5fa51c5fe9
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-17 17:24:57 +0200

    SpotifyCurrentTrack: add compact version

--- app/(main)/music/components/SpotifyCurrentTrack.tsx
+++ app/(main)/music/components/SpotifyCurrentTrack.tsx
@@ -4,12 +4,13 @@ import { getCurrentlyPlaying } from "../lib/actions";
 import formatDuration from "../lib/formatDuration";
 
 type Props = {
+  compact?: boolean;
   alwaysRender?: boolean;
   currentlyPlayingText?: React.ReactElement | string;
   hideSpotifyURI?: boolean;
 };
 
-export default async ({ alwaysRender, currentlyPlayingText, hideSpotifyURI }: Props) => {
+export default async ({ compact, alwaysRender, currentlyPlayingText, hideSpotifyURI }: Props) => {
   const currentlyPlayingObject = await getCurrentlyPlaying();
   const track = currentlyPlayingObject?.item ? (currentlyPlayingObject.item as SpotifyApi.TrackObjectFull) : null;
   if (!alwaysRender && (!currentlyPlayingObject || !currentlyPlayingObject.item)) return null;
@@ -17,14 +18,14 @@ export default async ({ alwaysRender, currentlyPlayingText, hideSpotifyURI }: Pr
   return (
     <div>
       {currentlyPlayingText}
-      <div className="flex items-center gap-5 rounded p-2 hover:bg-gray-300 dark:hover:bg-gray-300/10">
+      <div className={`flex items-center rounded ${compact ? "gap-3 p-1" : "gap-5 p-2"} hover:bg-gray-300 dark:hover:bg-gray-300/10`}>
         {/* Album image */}
-        <Image src={track?.album?.images[1].url ?? "/assets/images/spotifylogo.png"} className="rounded" alt="Spotify logo" width={80} height={80} />
+        <Image src={track?.album?.images[1].url ?? "/assets/images/spotifylogo.png"} className="rounded" alt="Spotify logo" width={compact ? 40 : 80} height={compact ? 40 : 80} />
 
         <div className="flex w-full flex-col text-start">
           <div className="flex items-center justify-between">
             {/* Track name */}
-            <span className="text-xl font-semibold">
+            <span className={`${compact ? "" : "text-xl"} font-semibold`}>
               {track ? (
                 <>
                   <Link href={track.external_urls.spotify} target="_blank" className="hover:underline">
@@ -32,7 +33,7 @@ export default async ({ alwaysRender, currentlyPlayingText, hideSpotifyURI }: Pr
                   </Link>
                   {!hideSpotifyURI && (
                     <Link href={track.uri} target="_blank" title="Open in Spotify" className="hover:underline">
-                      <i className="fa-brands fa-spotify fa-lg ms-2" />
+                      <i className={`fa-brands fa-spotify ${compact ? "" : "fa-lg"} ms-2`} />
                     </Link>
                   )}
                 </>
@@ -50,7 +51,7 @@ export default async ({ alwaysRender, currentlyPlayingText, hideSpotifyURI }: Pr
           </div>
 
           {/* Artist names */}
-          <span className="text-sm text-neutral-700 dark:text-neutral-300">
+          <span className={`${compact ? "text-xs" : "text-sm"} text-neutral-700 dark:text-neutral-300`}>
             {track?.artists
               .map<React.ReactNode>(artist => (
                 <Link key={artist.id} href={artist.external_urls.spotify} target="_blank" className="hover:underline">
