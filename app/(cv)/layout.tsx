7c294f727404c1e8eb839f5b2cc9aa3b6420e1d7
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-18 13:49:41 +0200

    cv: add metadataBase

--- app/(cv)/layout.tsx
+++ app/(cv)/layout.tsx
@@ -2,6 +2,10 @@ import "/public/assets/styles/cv.css";
 import "/public/fontawesome/css/fontawesome.min.css";
 import "/public/fontawesome/css/brands.min.css";
 
+export const metadata = {
+  metadataBase: new URL("https://leonlarsson.com")
+};
+
 export default ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
