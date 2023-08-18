575b21ef89bdeca69c9f55995f8113ad50650a34
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-18 22:20:29 +0200

    SpotifyRangeSelector: use <select> instead

--- app/(main)/music/components/SpotifyRangeSelector.tsx
+++ app/(main)/music/components/SpotifyRangeSelector.tsx
@@ -4,43 +4,20 @@ import { useRouter, useSearchParams } from "next/navigation";
 
 export default () => {
   const router = useRouter();
-  const searchParams = useSearchParams();
+  const range = useSearchParams().get("range");
+
+  const onChange = ({ target: { value } }: React.ChangeEvent<HTMLSelectElement>) => router.replace(value !== "medium_term" ? `?range=${value}` : "?");
 
   return (
     <div>
-      <span>Select a range for top tracks and artists:</span>
-      <div className="mt-1 flex flex-wrap gap-2">
-        {/* <select className="rounded p-2" name="Hello" onChange={e => router.replace(e.target.value !== "medium_term" ? `?range=${e.target.value}` : "?")}>
-        <option value="short_term">Short term</option>
-        <option value="medium_term">Medium term</option>
+      <label className="select-none" htmlFor="select">
+        Select a range for top tracks and artists:
+      </label>
+      <select className="mt-1 block w-full rounded border border-black/50 p-2 hover:border-black dark:border-white/50 dark:bg-kinda-black dark:hover:border-white" id="select" defaultValue={range ?? "medium_term"} onChange={onChange}>
+        <option value="short_term">Last 4 weeks</option>
+        <option value="medium_term">Last 6 months</option>
         <option value="long_term">Long term</option>
-      </select> */}
-
-        {/* <div>
-        <label htmlFor="short">Short term</label>
-        <input type="radio" name="range" id="short" />
-        </div>
-        
-        <div>
-        <label htmlFor="medium">Medium term</label>
-        <input type="radio" name="range" id="medium" checked defaultChecked={true} />
-        </div>
-        
-        <div>
-        <label htmlFor="long">Long term</label>
-        <input type="radio" name="range" id="long" />
-      </div> */}
-
-        <button className={`button-with-border transition-all disabled:cursor-not-allowed ${searchParams.get("range") === "short_term" ? "font-bold dark:border-kinda-white" : ""}`} onClick={() => router.replace("?range=short_term")} disabled={searchParams.get("range") === "short_term"}>
-          Last 4 weeks
-        </button>
-        <button className={`button-with-border transition-all disabled:cursor-not-allowed ${!searchParams.get("range") ? "font-bold dark:border-kinda-white" : ""}`} onClick={() => router.replace("?")} disabled={!searchParams.get("range")}>
-          Last 6 months
-        </button>
-        <button className={`button-with-border transition-all disabled:cursor-not-allowed ${searchParams.get("range") === "long_term" ? "font-bold dark:border-kinda-white" : ""}`} onClick={() => router.replace("?range=long_term")} disabled={searchParams.get("range") === "long_term"}>
-          Long term
-        </button>
-      </div>
+      </select>
     </div>
   );
 };
