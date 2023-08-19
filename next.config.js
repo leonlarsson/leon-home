1730d33201eb1c329717a75bebe6b3b44b25c73a
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-20 01:55:50 +0200

    config: add /resume redirect

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  redirects: async () => [
+    {
+      source: "/resume",
+      destination: "/cv",
+      permanent: true
+    }
+  ],
   images: {
     domains: ["i.scdn.co"]
   },
