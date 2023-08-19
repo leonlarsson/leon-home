34b290a88e4d82ae52468f43c76387377d4017cf
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-19 23:03:23 +0200

    projects: fix

--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -23,8 +23,14 @@ export const generateMetadata = ({ searchParams }: { searchParams: { search: str
 
 export default ({ searchParams }: { searchParams: Record<string, string> }) => {
   const searchParam = searchParams.search;
+
+  // Most nauseating code I've ever written
   const projects = searchParam
-    ? projectsData.filter(project => [project.slug, ...(project.slugAliases ?? []), project.name, project.description, project.shortDescription, project.year, project.link?.replace("https://", ""), ...(project.tags ?? [])].some(item => item?.toLowerCase().includes(searchParam?.toLowerCase())))
+    ? projectsData.filter(project =>
+        [project.slug, ...(project.slugAliases ?? []), project.name, ...(typeof project.description === "string" ? [project.description] : project.description), project.shortDescription, project.year, project.link?.replace("https://", ""), ...(project.tags ?? [])].some(item =>
+          item?.toLowerCase().includes(searchParam?.toLowerCase())
+        )
+      )
     : projectsData;
 
   return (
