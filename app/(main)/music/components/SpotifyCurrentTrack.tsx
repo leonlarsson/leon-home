e547805028e89c913248882038c845cb0810470a
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-20 16:14:33 +0200

    SpotifyCurrentTrack: fix up
    Add flex-shrink: 0 to avoid squished pre-images.
    
    Add track progress to skeleton.

--- app/(main)/music/components/SpotifyCurrentTrack.tsx
+++ app/(main)/music/components/SpotifyCurrentTrack.tsx
@@ -21,7 +21,7 @@ export default async ({ compact, alwaysRender, currentlyPlayingText, hideSpotify
       {currentlyPlayingText}
       <div className={`flex items-center rounded ${compact ? "gap-3 p-1" : "gap-5 p-2"} hover:bg-gray-300 dark:hover:bg-gray-300/10`}>
         {/* Album image */}
-        <Image src={track?.album?.images[1].url ?? "/assets/images/spotifylogo.png"} className="rounded" alt="Spotify logo" width={compact ? 40 : 80} height={compact ? 40 : 80} />
+        <Image src={track?.album?.images[1].url ?? "/assets/images/spotifylogo.png"} className="shrink-0 rounded" alt="Spotify logo" width={compact ? 40 : 80} height={compact ? 40 : 80} />
 
         <div className="flex w-full flex-col text-start">
           <div className="flex items-center justify-between">
@@ -67,10 +67,13 @@ export const CurrentTrackSkeleton = ({ compact, currentlyPlayingText }: { compac
   <div>
     {currentlyPlayingText}
     <div className={`flex items-center rounded ${compact ? "gap-3 p-1" : "gap-5 p-2"} hover:bg-gray-300 dark:hover:bg-gray-300/10`}>
-      <div className={`${compact ? "h-10 w-10" : "h-20 w-20"} rounded bg-neutral-400 dark:bg-neutral-700`}></div>
+      <div className={`${compact ? "h-10 w-10" : "h-20 w-20"} shrink-0 rounded bg-neutral-400 dark:bg-neutral-700`}></div>
 
-      <div className="flex flex-col text-start">
-        <span className={`${compact ? "" : "text-xl"} font-semibold`}>Track name</span>
+      <div className="flex w-full flex-col text-start">
+        <div className="flex items-center justify-between">
+          <span className={`${compact ? "" : "text-xl"} font-semibold`}>Track name</span>
+          <span className="text-sm text-neutral-700 dark:text-neutral-300">0:00 / 4:20</span>
+        </div>
         <span className={`${compact ? "text-xs" : "text-sm"} text-neutral-700 dark:text-neutral-300`}>Artist name</span>
       </div>
     </div>
