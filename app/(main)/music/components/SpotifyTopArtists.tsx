34fdcd0a2d6c6cf0273d21d024baa10965ef0c6d
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-21 23:16:42 +0200

    SpotifyTopArtists: full round

--- app/(main)/music/components/SpotifyTopArtists.tsx
+++ app/(main)/music/components/SpotifyTopArtists.tsx
@@ -16,8 +16,8 @@ export default async ({ range }: Props) => {
       {artists.map(artist => (
         <Link key={artist.id} href={artist.external_urls.spotify} target="_blank" className="flex items-center gap-3 rounded p-2 hover:bg-gray-300 dark:hover:bg-gray-300/10">
           {/* Album image */}
-          <div className="h-[50px] w-[50px] overflow-hidden rounded-full">
-            <Image src={artist.images[0].url} className="h-full w-full shrink-0 object-cover" alt={`Image for '${artist.name}'`} width={50.01} height={50.01} loading="lazy" />
+          <div className="relative h-[64px] w-[64px] overflow-hidden rounded-full">
+            <Image src={artist.images[0].url} className="relative h-full w-full shrink-0 object-cover" width={128} height={128} alt={`Image for '${artist.name}'`} loading="lazy" />
           </div>
 
           <div className="flex flex-col text-start">
