63e185aad6131c6a0a28c44d29bb1155b858ff8d
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-22 00:32:08 +0200

    GradientBorder: add hover shadow
    Mostly to increase contrast on light mode.

--- app/(main)/components/GradientBorder.tsx
+++ app/(main)/components/GradientBorder.tsx
@@ -1,6 +1,10 @@
 export default ({ rounded = "rounded", padding = "p-px", extraClasses = "", hoverable = false, from, via, to, hoverFrom, hoverVia, hoverTo, children }: Props) => {
   return (
-    <div className={`select-none ${rounded} bg-gradient-to-r ${from ?? "from-pink-500"} ${via ?? "via-red-500"} ${to ?? "to-yellow-500"} ${padding} ${hoverable ? `${hoverFrom ?? "hover:from-yellow-500"} ${hoverVia ?? "hover:via-green-600"} ${hoverTo ?? "hover:to-blue-600"}` : ""} ${extraClasses}`}>
+    <div
+      className={`select-none hover:drop-shadow-md ${rounded} bg-gradient-to-r ${from ?? "from-pink-500"} ${via ?? "via-red-500"} ${to ?? "to-yellow-500"} ${padding} ${
+        hoverable ? `${hoverFrom ?? "hover:from-yellow-500"} ${hoverVia ?? "hover:via-green-600"} ${hoverTo ?? "hover:to-blue-600"}` : ""
+      } ${extraClasses}`}
+    >
       {children}
     </div>
   );
