d49c1d54fb6ec282d2d86769aa291a3844badf6b
Leon San José Larsson <leonlarsson8@gmail.com>
2023-08-22 19:01:20 +0200

    formatDuration: update to match Spotify

--- app/(main)/music/lib/formatDuration.ts
+++ app/(main)/music/lib/formatDuration.ts
@@ -1,6 +1,12 @@
-export default (durationMs: number) => {
+export default (durationMs: number): string => {
   const totalSeconds = Math.floor(durationMs / 1000);
-  const minutes = Math.floor(totalSeconds / 60);
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
   const seconds = totalSeconds % 60;
-  return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+
+  if (hours > 0) {
+    return `${hours}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  } else {
+    return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+  }
 };
