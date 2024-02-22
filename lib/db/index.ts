feef8f7e165513338e87ca5dc0a83286039610be
Leon San José Larsson <leonlarsson8@gmail.com>
2024-02-22 19:34:26 +0100

    deps: update DB stuff

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,10 +1,10 @@
-import { connect } from "@planetscale/database";
+import { Client } from "@planetscale/database";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 
-const connection = connect({
+const client = new Client({
   host: process.env.DATABASE_HOST,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
 });
 
-export const db = drizzle(connection);
+export const db = drizzle(client);
