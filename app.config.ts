8586c5413129f42b26c0d5e5daaf8fbc50b8bb88
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2024-12-05 22:45:19 +0100

    i give up

--- app.config.ts
+++ app.config.ts
@@ -17,7 +17,6 @@ export default defineConfig({
     esbuild: {
       target: "esnext",
     },
-    define: await proxyCloudflareEnv(),
     plugins: [
       tsConfigPaths({
         projects: ["./tsconfig.json"],
@@ -25,35 +24,3 @@ export default defineConfig({
     ],
   },
 });
-
-async function proxyCloudflareEnv() {
-  if (isInCloudflareCI()) return undefined;
-
-  const env = await getCloudflareProxyEnv();
-
-  const viteDefine = Object.fromEntries(
-    Object.entries(env)
-      .filter(([key]) => key.startsWith("VITE_"))
-      .map(([key, value]) => [`import.meta.env.${key}`, `"${value}"`]),
-  );
-
-  return viteDefine;
-}
-
-function isInCloudflareCI() {
-  return process.env.CF_PAGES_COMMIT_SHA !== undefined;
-}
-
-async function getCloudflareProxyEnv() {
-  const { getPlatformProxy } = await import("wrangler");
-
-  const proxy = await getPlatformProxy({
-    environment: process.env.npm_lifecycle_event === "build" ? "production" : "development",
-  });
-
-  const cloudflareEnv = proxy.env;
-
-  await proxy.dispose();
-
-  return cloudflareEnv;
-}
