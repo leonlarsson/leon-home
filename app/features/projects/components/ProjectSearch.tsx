08f03bd2f91e0e70638a7ff20faaacaf99243ed5
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2024-12-06 03:38:22 +0100

    ProjectSearch: fix weird behaviour

--- app/features/projects/components/ProjectSearch.tsx
+++ app/features/projects/components/ProjectSearch.tsx
@@ -1,10 +1,16 @@
 import { GradientBorder } from "@/features/common/GradientBorder";
 import { getRouteApi } from "@tanstack/react-router";
+import { useEffect, useState } from "react";
 
 export const ProjectSearch = () => {
   const { useNavigate, useSearch } = getRouteApi("/_main/projects/");
   const { search } = useSearch();
   const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState(search || "");
+
+  useEffect(() => {
+    setInputValue(search || "");
+  }, [search]);
 
   return (
     <GradientBorder rounded="5px" padding="1px">
@@ -13,9 +19,10 @@ export const ProjectSearch = () => {
         maxLength={20}
         placeholder="Name, description, year, tags..."
         className="text-input-base w-full"
-        defaultValue={search}
+        value={inputValue}
         onChange={(e) => {
           const value = e.target.value.trim();
+          setInputValue(value);
 
           // If value is not empty, set searchParam. Otherwise, remove the searchParam. Then replace the URL params
           navigate({
