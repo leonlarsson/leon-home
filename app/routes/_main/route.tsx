c0a7f666a5c4d3468dd9b63edbbfb28651fcf8f8
Leon San José Larsson <54620188+leonlarsson@users.noreply.github.com>
2024-12-08 20:55:33 +0100

    main: adjust padding

--- app/routes/_main/route.tsx
+++ app/routes/_main/route.tsx
@@ -14,8 +14,8 @@ function RouteComponent() {
     <div className={"min-h-[100svh] bg-kinda-white text-black dark:bg-kinda-black dark:text-kinda-white"}>
       <Header />
       {/* Below 510px (no header) width, add bottom margin to accomodate the bottom overlay header. Add relative if issues occur */}
-      {/* Also below 510px width, add some padding to the top */}
-      <div className="container mx-auto px-4 pb-28 pt-3 text-center transition-all min-[510px]:pb-10 min-[510px]:pt-0">
+      {/* Also below 510px width, add some padding to the top, and decrease horizontal padding */}
+      <div className="container mx-auto px-4 pb-28 pt-3 text-center transition-all min-[510px]:px-6 min-[510px]:pb-10 min-[510px]:pt-0">
         <Outlet />
       </div>
     </div>
